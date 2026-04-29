73212ce5205e9f28240f39dcbc654c6e6c8d8733
jmo2k02 <justusmrosk@web.de>
2026-04-29 19:46:52 +0000

    added interacitve shell

--- travelplanner/src/travelplanner/cli/main.py
+++ travelplanner/src/travelplanner/cli/main.py
@@ -3,6 +3,7 @@
 import typer
 
 from .evaluation import app as evaluation_app
+from .interactive import app as interactive_app
 from .planner import app as planner_app
 
 app = typer.Typer(
@@ -10,6 +11,7 @@ app = typer.Typer(
     no_args_is_help=True,
     rich_markup_mode="rich",
 )
+app.add_typer(interactive_app, name="interactive", no_args_is_help=True)
 app.add_typer(planner_app, name="planner", no_args_is_help=True)
 app.add_typer(evaluation_app, name="eval", no_args_is_help=True)
 
